eb12c1696affe6387ea49f0d16bccface33db544
GGMU19 <joeburin19@gmail.com>
2025-05-01 15:58:47 -0400

    Initial Spring Boot setup for Event Photo Sharing Platform

---
+++ src/main/java/com/joeburin/event_photo_sharing/EventPhotoSharingApplication.java
@@ -0,0 +1,13 @@
+package com.joeburin.event_photo_sharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventPhotoSharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventPhotoSharingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joeburin/event_photo_sharing/EventPhotoSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joeburin.event_photo_sharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventPhotoSharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
